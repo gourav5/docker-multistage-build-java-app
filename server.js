0661999ff1ad69c2bf33a3468f65976f4252fc8a
gourav <gouravgaur270@gmail.com>
2025-09-09 15:09:01 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const http = require('http');
+
+const port = 4000;
+const server = http.createServer((req, res) => {
+  res.end('Welcome Gourav kumar in the Devops!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
