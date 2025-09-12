a1a8bd8de8e12c840532326006a39157b82c1120
gourav <gouravgaur270@gmail.com>
2025-09-12 10:38:16 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const port = 4000;
 const server = http.createServer((req, res) => {
-  res.end('Hello Gourav Kumar ! Welcome to the Devops!');
+  res.end('Hello Gourav Kumar ! Node Js app Working fine!');
 });
 
 server.listen(port, () => {
