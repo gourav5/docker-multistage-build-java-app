063d5cf02f95702841e84a9a2b3ef55c997b139a
gourav <gouravgaur270@gmail.com>
2025-09-12 11:03:39 +0530

    updated server.js file password and key

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // server.js
 const http = require('http');
 
+// Hardcoded secret (for testing vulnerability detection)
+const DB_PASSWORD = "SuperSecretPassword123!";
+const API_KEY = "AKIAIOSFODNN7EXAMPLE";
+
 const port = 4000;
 const server = http.createServer((req, res) => {
-  res.end('Hello Gourav Kumar ! Node Js app Working fine!');
+  res.end('Hello Gourav Kumar! Node.js app working fine!');
 });
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+  console.log("Database Password: " + DB_PASSWORD);
+  console.log("API Key: " + API_KEY);
 });
 
